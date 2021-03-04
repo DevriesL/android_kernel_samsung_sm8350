1398e9dab95f0f878cf821fc4b1ccecbc0b7605a
DevriesL <therkduan@gmail.com>
2021-03-04 23:17:14 +0800

    Update build script
    Signed-off-by: DevriesL <therkduan@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-export ARCH=arm64
 mkdir out
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
+BUILD_CROSS_COMPILE=/home/devries/Workspace/Kernel/Toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+CLANG_PATH=/home/devries/Workspace/Kernel/Toolchain/clang/clang-r383902/bin
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/p3q_chn_hkx_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
- 
+export ARCH=arm64
+export PATH=${CLANG_PATH}:${PATH}
+
+make -j56 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE \
+    CC=clang LD=ld.lld \
+    vendor/devries_defconfig
+
+make -j56 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE \
+    CC=clang LD=ld.lld \
+    2>&1 | tee build.txt
+
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
